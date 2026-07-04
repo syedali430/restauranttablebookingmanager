50545f5f2d3972a2aa89a72b3d560a45a2076a58
Syed Ali Dilawar Shah <syedalidilawarshah@gmail.com>
2026-07-04 04:14:15 +0200

    Create Booking controller

---
+++ src/main/java/com/example/restauranttablebookingmanager/controller/BookingController.java
@@ -0,0 +1,18 @@
+package com.example.restauranttablebookingmanager.controller;
+
+import com.example.restauranttablebookingmanager.repository.BookingRepository;
+import com.example.restauranttablebookingmanager.view.BookingView;
+
+public class BookingController {
+
+    private final BookingRepository repository;
+    private final BookingView view;
+
+    public BookingController(
+            BookingRepository repository,
+            BookingView view) {
+
+        this.repository = repository;
+        this.view = view;
+    }
+}
